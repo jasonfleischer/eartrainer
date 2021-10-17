1a4a645ef2b4e68ff3c66cf6b88fa2a8e58999ef
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-10-17 14:13:19 -0400

    update

--- js/main.js
+++ js/main.js
@@ -133,9 +133,9 @@ function window_resized_end(){
 		}
 	}
 
-	var column_width = getComputedStyle(document.documentElement).getPropertyValue("--left-column-width").replace("px", "")
+	var column_width = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--left-column-width").replace("px", ""));
 	let contentWidth = document.body.clientWidth - column_width;
-	log.e("contentWidth: " + contentWidth + ": "+ (typeof column_width) + " b;" + document.body.clientWidth + " , "+ $("content_view").style.marginLeft)
+	log.e("contentWidth: " + contentWidth + ": "+ (typeof contentWidth) + " b;" + document.body.clientWidth + " , "+ $("content_view").style.marginLeft)
 	$("init_view").style.width = contentWidth + "px";
 	$("answer_container").style.width = contentWidth + "px";
 
