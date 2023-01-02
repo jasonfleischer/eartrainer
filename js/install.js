30e5052c34cf266ba33c13c604c9dfc30933bc02
Jason Fleischer <jason_fleischer@hotmail.ca>
2023-01-02 12:59:19 -0500

    update

--- js/install.js
+++ js/install.js
@@ -42,15 +42,15 @@ install.showAlert = function(install_action){
 		<p>Install this app on your device to easily access it anytime. Installing this app will result in better performance, improved fullscreen experience, and usage without an internet connection.</p>
 		<br/>`
 	
-	var isSafariMobile = window.mobileAndTabletCheck() && isSafari
+	var isSafariMobile = window.mobileAndTabletCheck() && isSafari;
 	if (isSafariMobile){
 		contents += `
 			<div id="ios_install_instructions">
 				<p>1. Tap on <img src="img/export.png" alt="export"/></p>
 				<p>2. Select 'Add to Home Screen'</p>
-			</div>`
+			</div>`;
 	} else {
-		contents += '<button id="install">Install</button>'
+		contents += '<button id="install">Install</button>';
 	}
 
 	alert.show("Install App", contents)
