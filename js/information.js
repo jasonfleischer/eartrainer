72afca2cf5e544a81dd42813e42c08eee989c7b3
Jason Fleischer <jason_fleischer@hotmail.ca>
2023-01-02 21:50:03 -0500

    update

--- js/information.js
+++ js/information.js
@@ -2,18 +2,17 @@ var information = {}
 
 information.showAlert = function(){
 
-	let contents = `
-		<br class="desktop-only" />
-		<p id="openMailToDeveloper">Thank you for using this website. If you wish to submit feedback, comment or report an error click <strong>here</strong></p>
-		<br/>
-		<p id="surikov">Special thanks to Surikov for their <strong>WebAudioFont</strong> library.</p>
-		<br/>
-		<p id="personalWebsite">Information about the developer can be found <strong>here</strong>.</p>
-		<br/>
-		<p id="musicAppWebsite">Other music apps created by this developer can be found <strong>here</strong>.</p>
-		<br class="desktop-only"/>
-		<button class="desktop-only" id="keyboard_shortcuts">Keyboard Shortcuts</button>
-	`
+	let contents = 
+		'<br class="desktop-only" />'+
+		'<p id="openMailToDeveloper">Thank you for using this website. If you wish to submit feedback, comment or report an error click <strong>here</strong></p>'+
+		'<br/>'+
+		'<p id="surikov">Special thanks to Surikov for their <strong>WebAudioFont</strong> library.</p>'+
+		'<br/>'+
+		'<p id="personalWebsite">Information about the developer can be found <strong>here</strong>.</p>'+
+		'<br/>'+
+		'<p id="musicAppWebsite">Other music apps created by this developer can be found <strong>here</strong>.</p>'+
+		'<br class="desktop-only"/>'+
+		'<button class="desktop-only" id="keyboard_shortcuts">Keyboard Shortcuts</button>';
 	alert.show("Information", contents)
 	information.setupOnClicks();
 }
