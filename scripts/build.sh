e1e86644026242db3df09105f7362306d7fd66f5
Jason Fleischer <jason_fleischer@hotmail.ca>
2023-02-07 16:49:15 -0500

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,7 +14,7 @@ npm update;
 uglifycss css/alert.css css/button.css css/main.css css/root.css css/select.css css/slider.css css/switch.css > css/bundle.css
 uglifyjs js/model.js js/prototypes.js js/storage.js js/audio_controller.js js/alert.js js/information.js js/keyboard_shortcuts.js js/midi/midi_controller.js js/window_resize.js js/setup_controls.js js/main.js js/install.js service_worker.js -o js/bundle.js
 browserify js/bundle.js -o js/bundle.js
-uglifyjs js/bundle.js -o js/bundle.js
+#uglifyjs js/bundle.js -o js/bundle.js
 
 git add *; git commit -m 'update'; git push;
 
