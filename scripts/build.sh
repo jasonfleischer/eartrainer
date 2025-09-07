5f46a7bbac7b12c5dc25028e82837bf1e10c1989
Jason Fleischer <jason_fleischer@hotmail.ca>
2025-09-07 12:05:19 -0400

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,8 +11,8 @@ CWD=`pwd`
 
 npm update;
 
-uglifycss css/alert.css css/button.css css/main.css css/root.css css/select.css css/slider.css css/switch.css > css/bundle.css
-uglifyjs js/model.js js/prototypes.js js/storage.js js/audio_controller.js js/alert.js js/information.js js/keyboard_shortcuts.js js/midi/midi_controller.js js/window_resize.js js/setup_controls.js js/main.js js/install.js service_worker.js -o js/bundle.js
+uglifycss css/alert.css css/button.css css/main.css css/root.css css/select.css css/slider.css css/switch.css css/dual_control.css > css/bundle.css
+uglifyjs js/model.js js/prototypes.js js/storage.js js/audio_controller.js js/alert.js js/information.js js/keyboard_shortcuts.js js/midi/midi_controller.js js/window_resize.js js/setup_controls.js js/main.js js/install.js service_worker.js js/dual_range_control.js -o js/bundle.js
 browserify js/bundle.js -o js/bundle.js
 uglifyjs js/bundle.js -o js/bundle.js
 
